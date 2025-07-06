ab8991e7308dc9a7e0674374b7a2ee2fd3fb985a
JAHIDUL456 <1904015@student.ruet.ac.bd>
2025-07-06 13:55:16 +0600

    all page

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
     <div>
-        <Navbar />
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/about" element={<h1>About Page</h1>} /> 
+          <Route path="/contact" element={<h1>Contact Page</h1>} />
+          <Route path="/products" element={<h1>Products Page</h1>} />
+        </Routes>
+        
+      </BrowserRouter>
     </div>
   )
 }
