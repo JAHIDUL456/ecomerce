4684fc38ca48dcb3203003a0fa11dd16f93ca489
JAHIDUL456 <1904015@student.ruet.ac.bd>
2025-07-07 21:09:58 +0600

    lol

--- my-project/src/Components/Popular/Popular.jsx
+++ my-project/src/Components/Popular/Popular.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import data from '../../Components/Assets/data.js'
+
 const Popular = () => {
   return (
-    <div className='w-[90%] mx-auto  bg-white'>
-        <h2 className='text-3xl font-bold text-center my-8 font-serif  '>Popular Products</h2>
-        <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 p-2 mb-2 sm:m-[-50px]'>
-            {data.map((item) => (
-            <div key={item.id} className=' p-4 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out'>
-                <div className='flex justify-center'><img src={item.image} alt={item.name} className='w-[250px] mb-4 p-2 rounded-xl ' /></div>
-                <h3 className='text-lg font-semibold mb-2 sm:text-md'>{item.name}</h3>
-                <p className='text-gray-500 line-through'>${item.old_price.toFixed(2)}</p>
-                <p className='text-green-600 text-xl font-bold'>${item.new_price.toFixed(2)}</p>
+    <div className='w-[90%] mx-auto bg-white mb-25'>
+      <h2 className='text-3xl font-bold text-center font-serif p-2 lg:mb-12 mt-5'>
+        Popular Products
+      </h2>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 p-4'>
+        {data.map((item) => (
+          <div
+            key={item.id}
+            className='w-full p-2 rounded-lg shadow-lg hover:scale-105 transition-all duration-300 ease-in-out hover:bg-gray-100 hover:border-2 hover:border-gray-300'
+          >
+            <div className='flex justify-center'>
+              <img
+                src={item.image}
+                alt={item.name}
+                className='w-full max-w-[180px] h-auto mb-4 p-2 rounded-xl'
+              />
             </div>
-            ))}
-        </div>
-
+            <h3 className='text-lg font-semibold mb-2 sm:text-md'>{item.name}</h3>
+            <p className='text-gray-500 line-through'>${item.old_price.toFixed(2)}</p>
+            <p className='text-green-600 text-xl font-bold'>${item.new_price.toFixed(2)}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
