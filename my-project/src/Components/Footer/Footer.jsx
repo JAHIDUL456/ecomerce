ef22ea23055c4d2acf3b2fb782ddf8e016d3058f
JAHIDUL456 <1904015@student.ruet.ac.bd>
2025-07-10 11:08:36 +0600

    footer

--- my-project/src/Components/Footer/Footer.jsx
+++ my-project/src/Components/Footer/Footer.jsx
@@ -4,7 +4,17 @@ const Footer = () => {
   return (
     <div>
       {/* this section is for the footer */}
-      
+      <footer className='bg-gray-800 text-white py-6 mt-10'>
+        <div className='container mx-auto text-center'>
+          <p className='text-sm'>© 2025 Fashion_BD. All rights reserved.</p>
+          <p className='text-sm'>Follow us on:</p>
+          <div className='flex justify-center space-x-4 mt-2'>
+            <a href='#' className='text-white hover:text-gray-400'>Facebook</a>
+            <a href='#' className='text-white hover:text-gray-400'>Twitter</a>
+            <a href='#' className='text-white hover:text-gray-400'>Instagram</a>
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
