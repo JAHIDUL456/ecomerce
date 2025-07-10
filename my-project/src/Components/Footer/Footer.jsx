285d52d92242599e727e74db8f8ed5281abbb71d
JAHIDUL456 <1904015@student.ruet.ac.bd>
2025-07-10 16:44:45 +0600

    footer

--- my-project/src/Components/Footer/Footer.jsx
+++ my-project/src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       {/* this section is for the footer */}
-      <footer className='bg-gray-800 text-white py-6 mt-10'>
+      <footer className='bg-gray-500 text-white py-6 mt-10'>
         <div className='container mx-auto text-center'>
           <p className='text-sm'>© 2025 Fashion_BD. All rights reserved.</p>
           <p className='text-sm'>Follow us on:</p>
